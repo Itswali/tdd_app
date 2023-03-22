0ef683212b3da024d72617005ab66ad117c82c50
Wali Muhammad <luckywali0986@gmail.com>
2023-03-23 00:29:58 +0500

    add mock rspec test for fizzbuzz program

---
+++ spec/solver_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../solver.rb'
+
+describe Solver do
+  describe "#factorial" do
+    context "when given 0" do
+      it "returns 1" do
+        expect(Solver.new.factorial(0)).to eq(1)
+      end
+    end
+
+    context "when given a positive integer" do
+      it "returns the correct factorial" do
+        expect(Solver.new.factorial(5)).to eq(120)
+        expect(Solver.new.factorial(10)).to eq(3628800)
+      end
+    end
+
+    context "when given a negative integer" do
+      it "raises an exception" do
+        expect{Solver.new.factorial(-3)}.to raise_error(ArgumentError)
+      end
+    end
+  end
+end
